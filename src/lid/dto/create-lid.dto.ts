e7172e3198c4579416e101e7fae9c967e6a10472
din-sus <tuhtamatovdinmuhammad2@gmail.com>
2024-12-04 23:40:26 +0500

    added swagger

--- src/lid/dto/create-lid.dto.ts
+++ src/lid/dto/create-lid.dto.ts
@@ -1,14 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString } from "class-validator"
 
 export class CreateLidDto {
+    @ApiProperty({
+        description: 'fullname of the user',
+        example: 'Eshmat Toshmatov',
+        required: true
+    })
     @IsString()
     @IsNotEmpty()
     fullname: string
 
+    @ApiProperty({
+        description: 'Phone number of the user',
+        example: '+998975453232',
+        required: true
+    })
     @IsNotEmpty()
     @IsString()
     phoneNumber: string
 
+    @ApiProperty({
+        description: 'Branch where user studies',
+        example: 'Chilonzor',
+        required: true
+    })
     @IsString()
     @IsNotEmpty()
     branch: string
--- src/lid/lid.controller.ts
+++ src/lid/lid.controller.ts
@@ -2,31 +2,47 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { LidService } from './lid.service';
 import { CreateLidDto } from './dto/create-lid.dto';
 import { UpdateLidDto } from './dto/update-lid.dto';
+import { ApiBadRequestResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('lid')
 export class LidController {
   constructor(private readonly lidService: LidService) {}
 
+  @ApiOperation({summary: 'Saving users info, in LID table'})
+  @ApiOkResponse({description: "Successfully sended✅, wait until the Administrator will call you", type: CreateLidDto})
+  @ApiBadRequestResponse({description: 'You already sended a message❗'})
   @Post('create')
   create(@Body() createLidDto: CreateLidDto) {
     return this.lidService.create(createLidDto);
   }
 
+  @ApiOperation({summary: 'Getting users calls from LID table'})
+  @ApiOkResponse({description: "Found successfully", type: CreateLidDto})
+  @ApiBadRequestResponse({description: 'Data not found'})
   @Get()
   findAll() {
     return this.lidService.findAll();
   }
 
+  @ApiOperation({summary: 'Getting one exact users info from LID table'})
+  @ApiOkResponse({description: "Found", type: CreateLidDto})
+  @ApiBadRequestResponse({description: 'Data not found'})
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.lidService.findOne(+id);
   }
 
+  @ApiOperation({summary: 'Updating users info, in LID table'})
+  @ApiOkResponse({description: "Successfully updated", type: CreateLidDto})
+  @ApiBadRequestResponse({description: 'There is no such user'})
   @Patch('update/:id')
   update(@Param('id') id: string, @Body() updateLidDto: UpdateLidDto) {
     return this.lidService.update(+id, updateLidDto);
   }
 
+  @ApiOperation({summary: 'Deleting users info, in LID table'})
+  @ApiOkResponse({description: "Successfully deleted", type: CreateLidDto})
+  @ApiBadRequestResponse({description: 'There is no such user'})
   @Delete('delete/:id')
   remove(@Param('id') id: string) {
     return this.lidService.remove(+id);
